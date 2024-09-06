31101c11e777b7cd0cce9cc791f04ec63451ed7e
RodrigoBP00 <rodrigobpaiva@hotmail.com>
2024-09-06 18:07:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Esquilo vermelho",
+        descricao: "O esquilo de barriga vermelha é uma criatura adorável e ágil, conhecida por sua pelagem distintiva e seus hábitos arborícolas. Com uma barriga vibrante e uma cauda longa e espessa, ele habita florestas e áreas arborizadas, onde se alimenta principalmente de nozes, sementes e frutas. Sua agilidade e habilidade para escalar árvores o tornam um mestre em escapar de predadores. Além de sua beleza, o esquilo de barriga vermelha desempenha um papel importante na dispersão de sementes, contribuindo para a regeneração das florestas.",
+        link: "https://www.nationalgeographicbrasil.com/animais/2024/01/dia-mundial-do-esquilo-conheca-5-especies-de-esquilos-que-habitam-a-america-latina"
+        tags: "esquilo barriga vermelha"
+    },
+    {
+        titulo: "Esquilo cinzento",
+        descricao: "O esquilo cinzento é uma espécie comum na América do Norte, conhecido por sua pelagem cinzenta e sua capacidade de armazenar alimentos.",
+        link: "https://animalia.bio/pt/eastern-gray-squirrel"
+        tags: "cinzenta america do norte  alimentos"
+    },
+    {
+        titulo: "Esquilo voador",
+        descricao: "O esquilo voador é uma espécie noturna conhecida por suas membranas que se estendem entre os membros, permitindo que planeie entre as árvores.",
+        link: "https://www.nationalgeographicbrasil.com/animais/2021/06/descobertas-duas-novas-especies-de-esquilo-voador-do-tamanho-de-gatos-no-himalaia"
+        tags: "voador noturna membrana"
+    },
+    {
+        titulo: "Esquilo-Gigante-Indiano",
+        descricao: "Esses arborícolas esquilode quase dois quilos, nativos da Índia, têm um método incomum de armazenar os alimentos, diferente de qualquer outro esquilo.",
+        link: "https://www.nationalgeographicbrasil.com/animais/2019/04/esquilos-roxos-gigantes-existem-mesmo-e-eles-tem-um-comportamento-estranho"
+        tags: "india diferente gigante"
+    },
+    {
+        titulo: "Esquilo-Terrestre",
+        descricao: "O Spermophulis citellus é uma pequena espécie de esquilo terrestre que se caracteriza por formas arredondadas, grandes olhos protuberantes e cauda curta. Vive em colônias escavando galerias subterrâneas para formar sua toca.",
+        link: "https://animalia.bio/pt/marmotini"
+        tags: "pequena terrestre toca"
+    }
+];
